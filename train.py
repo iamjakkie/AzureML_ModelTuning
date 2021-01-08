6d507cf5d8b73524d5996b6764bf7a1de335b1ed
Jakkie <jakubkielbasiewicz@gmail.com>
2021-01-08 23:12:19 +0100

    Fix clean_data method

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
 
 def main():
     path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
